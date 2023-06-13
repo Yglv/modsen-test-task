71a46410f87202faba24b13bf41a38b5028ec2b4
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-06-13 23:23:31 +0300

    add meetup service

--- src/meetups/services/meetups/meetups.service.ts
+++ src/meetups/services/meetups/meetups.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Meetup } from 'src/typeorm/meetup.entity';
+import { MeetupDto } from 'src/meetups/dtos/meetups.dto';
 
 @Injectable()
-export class MeetupsService {}
+export class MeetupsService {
+  constructor(
+    @InjectRepository(Meetup)
+    private readonly meetupRepository: Repository<Meetup>,
+  ) {}
+
+  createMeetup(meetupDto: MeetupDto) {
+    const newMeetup = this.meetupRepository.create(meetupDto);
+    return this.meetupRepository.save(newMeetup);
+  }
+
+  findMeetups() {
+    return this.meetupRepository.find();
+  }
+
+  findMeetupById(id: number) {
+    return this.meetupRepository.findOneBy({ id: id });
+  }
+
+  changeMeetup(id: number) {
+    
+  }
+
+  deleteMeetup(id: number) {
+
+  }
+}
