3ae7e3f7f61b4cbeb78fecc4349ebf200edc5c7f
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-06-13 23:22:56 +0300

    add end points

--- src/meetups/controllers/meetups/meetups.controller.ts
+++ src/meetups/controllers/meetups/meetups.controller.ts
@@ -1,5 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UsePipes,
+  ValidationPipe,
+  Patch,
+  Delete
+} from '@nestjs/common';
+import { MeetupDto } from 'src/meetups/dtos/meetups.dto';
+import { MeetupsService } from 'src/meetups/services/meetups/meetups.service';
 
 @Controller('meetups')
 export class MeetupsController {
+  constructor(private readonly meetupsService: MeetupsService) {}
+
+  @Post()
+  create(@Body() createMeetupDto: MeetupDto) {
+    return this.meetupsService.createMeetup()
+  }
+
+  @Get()
+  findAll() {}
+
+  @Get(':id')
+  findOne(@Param(':id') id: number) {}
+
+  @Patch(':id')
+  update(@Param(':id') id: number, @Body() updateMeetupDto: MeetupDto) {
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {}
 }
+
