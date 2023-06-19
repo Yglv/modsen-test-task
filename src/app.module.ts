7437d2154a9df92bb424675bba4ca332335b42c0
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-06-19 22:15:34 +0300

    fix: add config validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
-import { MeetupsModule } from './meetups/meetups.module';
-import { UsersModule } from './users/users.module';
+import { MeetupsModule } from './modules/meetups/meetups.module';
+import { UsersModule } from './modules/users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import entities from './typeorm/index'
+import entities from './common/typeorm/index';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
