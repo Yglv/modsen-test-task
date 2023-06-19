5e0b0897efd8e9a3d83c2bafebae8142f55457d5
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-06-19 22:10:24 +0300

    feat: add seeder

---
+++ src/modules/meetups/seeders/meetups.seeder.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { InjectRepository } from '@nestjs/typeorm';
+import { DataFactory, Seeder } from 'nestjs-seeder';
+import { Meetup } from '../entities/meetup.entity';
+import { Repository } from 'typeorm';
+
+export class MeetupSeeder implements Seeder {
+  constructor(
+    @InjectRepository(Meetup)
+    private readonly meetupRepository: Repository<Meetup>,
+  ) {}
+
+  seed(): Promise<any> {
+    const meetups = DataFactory.createForClass(Meetup).generate(50);
+    return this.meetupRepository.insert(meetups);
+  }
+
+  drop(): Promise<any> {
+    return this.meetupRepository.delete({});
+  }
+}
