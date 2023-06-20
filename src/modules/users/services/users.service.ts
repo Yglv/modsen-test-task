2013810384142d0ef2723bfa13e3db12609c93f6
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-06-20 22:37:48 +0300

    feat: add user service

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserDto } from '../dtos/users.dto';
+import { User } from '../entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  logger: Logger;
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {
+    this.logger = new Logger();
+  }
+
+  async createUser(userDto: UserDto) {
+    const newUser = this.userRepository.create(userDto);
+    return await this.userRepository.save(newUser);
+  }
+
+  async findAllUsers() {
+    return await this.userRepository.find();
+  }
+
+  async findUserById(id: number) {
+    return await this.userRepository.findOneBy({ id: id });
+  }
+
+  async findUserByUsername(username: string) {
+    return await this.userRepository.findOneBy({ username: username });
+  }
+
+  async updateUserInfo(id: number, userDto: UserDto) {
+    return await this.userRepository.update(id, userDto);
+  }
+
+  async deleteUser(id: number) {
+    return await this.userRepository.delete(id);
+  }
+}
