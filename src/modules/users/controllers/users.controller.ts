53521b05e2badc8cedea8267fdc4717b7430d48c
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-06-20 22:36:38 +0300

    feat: add user controller

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,4 +1,90 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Logger,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UserDto } from '../dtos/users.dto';
+import { UsersService } from '../services/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  logger: Logger;
+  constructor(private readonly usersService: UsersService) {
+    this.logger = new Logger();
+  }
+
+  @Post()
+  @HttpCode(200)
+  async create(@Body() createUserDto: UserDto) {
+    const user = await this.usersService.createUser(createUserDto);
+    this.logger.log(user);
+    return user;
+  }
+
+  @Get()
+  @HttpCode(200)
+  async findAll() {
+    const users = await this.usersService.findAllUsers();
+    this.logger.log(users);
+    if (!users) {
+      throw new HttpException('Not found', HttpStatus.BAD_REQUEST);
+    }
+    return users;
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  async findById(@Param('id', ParseIntPipe) id: number) {
+    const user = await this.usersService.findUserById(id);
+    this.logger.log(user);
+    if (!user) {
+      throw new HttpException('Not found', HttpStatus.BAD_REQUEST);
+    }
+    return user;
+  }
+
+  @Get(':name')
+  @HttpCode(200)
+  async findByUsername(@Param('name') username: string) {
+    const user = await this.usersService.findUserByUsername(username);
+    this.logger.log(user);
+    if (!user) {
+      throw new HttpException('Not found', HttpStatus.BAD_REQUEST);
+    }
+    return user;
+  }
+
+  @Patch(':id')
+  @HttpCode(200)
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UserDto,
+  ) {
+    const user = await this.usersService.findUserById(id);
+    this.logger.log(user);
+    if (!user) {
+      throw new HttpException('Not found', HttpStatus.BAD_REQUEST);
+    }
+    return this.usersService.updateUserInfo(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(200)
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    const user = await this.usersService.findUserById(id);
+    this.logger.log(user);
+    if (!user) {
+      throw new HttpException('Not found', HttpStatus.BAD_REQUEST);
+    }
+    return this.usersService.deleteUser(id);
+  }
+}
