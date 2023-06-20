98f4af38fce200b03481177903ec63a84acdb36e
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-06-20 22:37:06 +0300

    feat: add user module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './controllers/users/users.controller';
-import { UsersService } from './services/users/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './controllers/users.controller';
+import { User } from './entities/user.entity';
+import { UsersService } from './services/users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
