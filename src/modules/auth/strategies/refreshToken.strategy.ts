7f754057df1e3277a41a30f1c45c447e025b9857
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-06-20 22:36:07 +0300

    feat: add refresh token strategy

---
+++ src/modules/auth/strategies/refreshToken.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+
+type jwtPayload = {
+  sub: string;
+  username: string;
+};
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('JWT_REFRESH_SECRET'),
+      passReqToCallback: true,
+    });
+  }
+
+  validate(req: Request, payload: jwtPayload) {
+    const refreshToken = req.get('Authorization').replace('Bearer', '').trim();
+    return { ...payload, refreshToken };
+  }
+}
