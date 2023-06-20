892e887d96d6a1f167e7aa6619dfdb128651b62e
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-06-20 22:33:31 +0300

    feat: add user entity

--- src/common/typeorm/index.ts
+++ src/common/typeorm/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
+import { User } from 'src/modules/users/entities/user.entity';
 import { Meetup } from '../../modules/meetups/entities/meetup.entity';
 
-const entities = [Meetup];
+const entities = [Meetup, User];
 
 export default entities;
