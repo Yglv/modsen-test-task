43199ae259438288254c30a416a425ebf8788410
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-06-21 22:07:02 +0300

    feat: add guards

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Get, Logger, Post, Req } from '@nestjs/common';
-import { UserDto } from '../users/dtos/users.dto';
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { UserDto } from '../users/dto/users.dto';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { Request } from 'express';
+import { AccessTokenGuard } from 'src/common/guards/accessToken.guard';
+import { RefreshTokenGuard } from 'src/common/guards/refreshToken.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -22,6 +32,15 @@ export class AuthController {
     return this.authService.signIn(authDto);
   }
 
+  @Get('refresh')
+  @UseGuards(RefreshTokenGuard)
+  refreshTokens(@Req() request: Request) {
+    const userId = request.user['sub'];
+    const refreshToken = request.user['refreshToken'];
+    return this.authService.refreshTokens(userId, refreshToken);
+  }
+
+  @UseGuards(AccessTokenGuard)
   @Get('logout')
   logout(@Req() request: Request) {
     this.authService.logout(request.user['sub']);
