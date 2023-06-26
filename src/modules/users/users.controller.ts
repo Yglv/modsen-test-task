79603bcda7a07ec024def592b82fed88e82da12a
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-06-26 23:47:34 +0300

    feat: add swagger

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -13,14 +13,17 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AccessTokenGuard } from 'src/common/guards/accessToken.guard';
 import { DeleteResult, UpdateResult } from 'typeorm';
+import { Meetup } from '../meetups/meetups.entity';
 import { UserDto } from './dto/users.dto';
 import { User } from './users.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
-@UseGuards(AccessTokenGuard)
+@ApiTags('users')
+//@UseGuards(AccessTokenGuard)
 export class UsersController {
   logger: Logger;
   constructor(private readonly usersService: UsersService) {
@@ -29,6 +32,10 @@ export class UsersController {
 
   @Post()
   @HttpCode(200)
+  @ApiResponse({ status: 200, description: 'create a user', type: User })
+  @ApiBody({
+    type: [UserDto],
+  })
   async create(@Body() createUserDto: UserDto): Promise<User> {
     const user = await this.usersService.createUser(createUserDto);
     this.logger.log(user);
@@ -37,6 +44,7 @@ export class UsersController {
 
   @Get()
   @HttpCode(200)
+  @ApiResponse({ status: 200, description: 'get all users', type: [User] })
   async findAll(): Promise<User[]> {
     const users = await this.usersService.findAllUsers();
     this.logger.log(users);
@@ -48,6 +56,7 @@ export class UsersController {
 
   @Get(':id')
   @HttpCode(200)
+  @ApiResponse({ status: 200, description: 'get a user by id', type: User })
   async findById(@Param('id', ParseIntPipe) id: number): Promise<User> {
     const user = await this.usersService.findUserById(id);
     this.logger.log(user);
@@ -59,6 +68,11 @@ export class UsersController {
 
   @Get(':name')
   @HttpCode(200)
+  @ApiResponse({
+    status: 200,
+    description: 'get a user by username',
+    type: User,
+  })
   async findByUsername(@Param('name') username: string): Promise<User> {
     const user = await this.usersService.findUserByUsername(username);
     this.logger.log(user);
@@ -70,6 +84,10 @@ export class UsersController {
 
   @Patch(':id')
   @HttpCode(200)
+  @ApiResponse({ status: 200, description: 'update a user', type: User })
+  @ApiBody({
+    type: [UserDto],
+  })
   async update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UserDto,
@@ -84,6 +102,7 @@ export class UsersController {
 
   @Delete(':id')
   @HttpCode(200)
+  @ApiResponse({ status: 200, description: 'delete a user', type: User })
   async delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
     const user = await this.usersService.findUserById(id);
     this.logger.log(user);
